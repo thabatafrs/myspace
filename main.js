b9cba08b69aa44380761deb95955daaeb9a5efa1
Thabata <thabatafreitas10@gmail.com>
2023-07-20 22:30:54 -0300

    theme att

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ const themeModal = document.querySelector('.customize-theme');
 const fontSizes = document.querySelectorAll('.choose-size span');
 
 var root = document.querySelector(':root');
+const colorPalette = document.querySelectorAll('.choose-color span');
+const Bg1 = document.querySelector('.bg-1');
+const Bg2 = document.querySelector('.bg-2');
+const Bg3 = document.querySelector('.bg-3');
 
 const changeActiveItem = () => {
     menuItems.forEach(item => {
@@ -49,23 +53,97 @@ fontSizes.forEach(size => {
         let fontSize;
         size.classList.toggle('active');
 
-        if (size.classList.contains('.font-size-1')) {
+        if (size.classList.contains('font-size-1')) {
             fontSize = '10px';
             root.style.setProperty('--sticky-top-left', '5.4rem');
-        } else if (size.classList.contains('.font-size-2')) {
+        } else if (size.classList.contains('font-size-2')) {
             fontSize = '13px';
             root.style.setProperty('--sticky-top-left', '5.4rem');
-        } else if (size.classList.contains('.font-size-3')) {
+        } else if (size.classList.contains('font-size-3')) {
             fontSize = '16px';
             root.style.setProperty('--sticky-top-left', '-2rem');
-        } else if (size.classList.contains('.font-size-4')) {
+        } else if (size.classList.contains('font-size-4')) {
             fontSize = '19px';
             root.style.setProperty('--sticky-top-left', '-5rem');
-        } else if (size.classList.contains('.font-size-5')) {
+        } else if (size.classList.contains('font-size-5')) {
             fontSize = '22px';
             root.style.setProperty('--sticky-top-left', '-10rem');
         }
 
         document.querySelector('html').style.fontSize = fontSize;
     })
+})
+
+
+
+const ChangeActiveColorClass = () => {
+    colorPalette.forEach(colorPicker => {
+        colorPicker.classList.remove('active');
+    })
+}
+
+colorPalette.forEach(color => {
+    color.addEventListener('click', () => {
+        let primaruHue;
+        ChangeActiveColorClass();
+
+        if (color.classList.contains('color-1')) {
+            primaruHue = 252;
+        } else if (color.classList.contains('color-2')) {
+            primaruHue = 52;
+        } else if (color.classList.contains('color-3')) {
+            primaruHue = 352;
+        } else if (color.classList.contains('color-4')) {
+            primaruHue = 152;
+        } else if (color.classList.contains('color-5')) {
+            primaruHue = 202;
+        }
+
+        color.classList.add('active');
+        root.style.setProperty('--primary-color-hue', primaruHue);
+    })
+})
+
+let lightColorLightness;
+let whiteColorLightness;
+let darkColorLightness;
+
+const changeBG = () => {
+    root.style.setProperty('--light-color-lightness', lightColorLightness);
+    root.style.setProperty('--dark-color-lightness', darkColorLightness);
+    root.style.setProperty('--white-color-lightness', whiteColorLightness);
+}
+
+Bg1.addEventListener('click', () => {
+
+    Bg1.classList.add('active');
+
+    Bg2.classList.remove('active');
+    Bg3.classList.remove('active');
+
+    window.location.reload();
+})
+
+Bg2.addEventListener('click', () => {
+    darkColorLightness = '95%';
+    whiteColorLightness = '20%';
+    lightColorLightness = '15%';
+
+    Bg2.classList.add('active');
+
+    Bg1.classList.remove('active');
+    Bg3.classList.remove('active');
+    changeBG();
+})
+
+Bg3.addEventListener('click', () => {
+    darkColorLightness = '95%';
+    whiteColorLightness = '10%';
+    lightColorLightness = '0%';
+
+    Bg3.classList.add('active');
+
+    Bg1.classList.remove('active');
+    Bg2.classList.remove('active');
+    changeBG();
 })
